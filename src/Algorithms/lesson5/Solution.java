03f5ec70b16809c16ef77d5d64f3d49cce3f1d43
MobyDrake <milkky598@yandex.ru>
2019-08-22 18:21:56 +0500

    homework

--- src/Algorithms/lesson5/Solution.java
+++ src/Algorithms/lesson5/Solution.java
@@ -1,12 +1,29 @@
 package Algorithms.lesson5;
 
+import Algorithms.lesson5.backPack.BackPack;
+
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Solution {
 
     public static void main(String[] args) {
-        new Anagram("cat").getAnagram();
+        long time = System.currentTimeMillis();
+        BackPack backPack = new BackPack(8);
+        ArrayList<BackPack.Item> list = new ArrayList<>();
+        list.add(new BackPack.Item("Книга", 1, 600));
+        list.add(new BackPack.Item("Бинокль", 2, 5000));
+        list.add(new BackPack.Item("Аптечка", 4, 1500));
+        list.add(new BackPack.Item("Ноутбук", 2, 40000));
+        list.add(new BackPack.Item("Котелок", 1, 500));
+
+        backPack.makePools(list);
+        backPack.displayContains();
+
+        System.out.println();
+        System.out.println(System.currentTimeMillis() - time);
+
     }
 
     private static int factorial(int n) {
@@ -29,5 +46,16 @@ public class Solution {
         }
     }
 
+    private static int sqr(int n, int s) {
+        if (s == 1) {
+            return n;
+        }
+
+        if (s < 0) {
+            return 1;
+        }
+
+        return  n * sqr(n, --s);
+    }
 
 }
